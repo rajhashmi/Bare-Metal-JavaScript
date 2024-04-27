22ae915179114002db965e5d22e3ab161937833d
shahid <shahidhashmiraj@gmail.com>
2024-04-27 22:07:24 +0530

    introduction

---
+++ introduction.js
@@ -0,0 +1,13 @@
+/*
+    Things we gonna cover in this repository.
+
+    1. Introduction to VMs
+    2. CPU - Understanding the physical machine
+    3. VMs - Array, Object, Funtions and ProtoType Chain
+    4. DeoptExplorer: Collecting data from the VM
+    5. Deopt: Calling Convention and Inlining 
+    6. Megamorhpism & Inline Cache: Object properties
+    7. Holes Arrays: Prototype chain resolution
+    8. == vs ===: `ValueOf()`;
+
+*/  
